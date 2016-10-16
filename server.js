a4f4e79b049adeb7f06f27628fde02a54f5bb908
Merquis Jesús Cruz Veloz <alu0100536690@ull.edu.es>
2016-10-16 19:21:31 +0100

    cambiando directorio

--- server.js
+++ server.js
@@ -9,7 +9,7 @@
 
 
    // ruteo
-   app.use(express.static(path.join(__dirname,'gh-pages')));
+   app.use(express.static(path.join(__dirname,'public')));
 
    app.get('/', function(request, response) {
      response.send('index');  
