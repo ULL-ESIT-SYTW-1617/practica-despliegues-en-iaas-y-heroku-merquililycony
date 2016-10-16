e6a95168d712911ab11ad3a294e72d120dd92943
Merquis Jesús Cruz Veloz <alu0100536690@ull.edu.es>
2016-10-16 19:19:04 +0100

    sincronizando iaas con gh-pages

---
+++ server.js
@@ -0,0 +1,25 @@
+// importar
+    var express = require('express');
+     
+    // instanciar
+    var app = express();
+    var path = require('path');
+    
+    var exec = require('child_process').exec; 
+
+
+   // ruteo
+   app.use(express.static(path.join(__dirname,'gh-pages')));
+
+   app.get('/', function(request, response) {
+     response.send('index');  
+   });
+     
+    // escuchar
+   // app.listen(8080);
+   app.set('port', process.env.PORT || 8080);
+
+    
+app.listen(app.get('port'), function() {
+  console.log('Servidor escuchando en el puerto:'+app.get('port'));
+});
